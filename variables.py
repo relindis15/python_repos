3f6dcf32e51e21843a6ba290db915b624c1eb7cd
relindis15 <onarirelindisblessed@gmail.com>
2022-12-12 23:18:37 -0500

    strings added

--- variables.py
+++ variables.py
@@ -1,8 +1,24 @@
-# a = 20  # declaring a variable a
-# b = 5
-# print(a/b)
-# print(a*b)
-a = input("enter the value of a")
-b = input("enter the value of b")
-sum = a+b
-print("the sum of the two numbers is", sum)
+x = 10
+print(x)
+type(x)
+print(type(x))  # printing out the data type of variable x
+y = 10.7
+print(type(y))  # printing out the data (type) of variable y
+x = "name"
+print("name")
+print(type(x))
+# x = 10
+# y = 20
+# z = 30
+(x, y, z) = 10, 20, 30
+print(x, y, z)
+p = [5, 10, 20, 30, 40]
+print(p)
+print(type(p))
+print(p[2])
+print(p[0])
+g = ["dog", "cat", "pig", "tiger", "lion", "goat"]
+print(g)
+print(type(g))
+print(g[2])
+print(g[5])
