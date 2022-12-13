d81924cd089edf655bfde0cb0678636b8af90762
relindis15 <onarirelindisblessed@gmail.com>
2022-12-12 19:39:29 -0500

    declaring variables

---
+++ variables.py
@@ -0,0 +1,8 @@
+# a = 20  # declaring a variable a
+# b = 5
+# print(a/b)
+# print(a*b)
+a = input("enter the value of a")
+b = input("enter the value of b")
+sum = a+b
+print("the sum of the two numbers is", sum)
