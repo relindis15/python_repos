915afdc314daed40f9f0f468b3b125f4f96eff60
relindis15 <onarirelindisblessed@gmail.com>
2022-12-14 22:36:31 -0500

    using input commands in python

---
+++ add_variable.py
@@ -0,0 +1,28 @@
+# store numbers inputed by the user
+a = input("enter the first number : ")
+b = input("enter the second number : ")
+
+# add the numbers
+sum = float(a) + float(b)
+
+# print result
+print("the sum of {0} and {1} is {2}".format(a, b, sum))
+
+# subtract the numbers
+
+sub = float(a) - float(b)
+
+print("the sub of {0} and {1} is {2}".format(a, b, sub))
+
+# divide the numbers
+
+div = float(a) / float(b)
+
+# print results
+print("the div of {0} and {1} is {2}".format(a, b, div))
+
+# multiply the numbers
+mul = float(a) * float(b)
+
+# print results
+print("the mul of {0} and {1}is {2}".format(a, b, mul))
