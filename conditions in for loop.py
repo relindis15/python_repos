1868a256ff29204229b9f3703c86ea05859c380f
Relindis <onarirelindisblessed@gmail.com>
2023-01-09 22:34:50 -0500

    commit

---
+++ conditions in for loop.py
@@ -0,0 +1,5 @@
+for i in range(1, 11):
+    if i % 2 == 0:
+        print('even numbers:', i)
+    else:
+        print('odd number:', i)
---
+++ square.py
@@ -0,0 +1,3 @@
+# for num in range (5)
+for num in range(1, 6):
+    print(num ** 2)
