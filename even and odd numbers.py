68a9a178dec4adc4f144ea13c40f3059396bac69
Relindis <onarirelindisblessed@gmail.com>
2023-01-11 23:15:18 -0500

    while loop exercises

---
+++ even and odd numbers.py
@@ -0,0 +1,9 @@
+# print even and odd numbers between1 to the entered number
+num = 20
+while num > 0:
+    # if else statement
+    if num % 2 == 0:
+        print(num, "is even")
+    else:
+        print(num, "is odd")
+    num = num-1
---
+++ while dividing.py
@@ -0,0 +1,7 @@
+# dividing a number by three to be less than or equal to 10
+count = 0
+num = 61
+while num >= 10:
+    num = num/3
+    count = count+1
+print(count)
