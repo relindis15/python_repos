1f654f22c93b4c4d6a01574760a47d38776ace4b
Relindis <onarirelindisblessed@gmail.com>
2023-01-11 21:33:31 -0500

    correction

--- iteration current and previous numbers.py
+++ iteration current and previous numbers.py
@@ -4,7 +4,7 @@ previous_num = 0
 # loop from 1 to 10
 for i in range(1, 11):
     x_sum = previous_num+i
-    print("Current Number", i, "Previous Number ",
+    print("Current_Number", i, "Previous_Number ",
           previous_num, "Sum: ", x_sum)
 # modify previous number
 # set it to the current number
