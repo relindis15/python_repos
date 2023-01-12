12152336228ff534a8a6a5507bd0f3026296e215
Relindis <onarirelindisblessed@gmail.com>
2023-01-11 20:32:39 -0500

    correction on average

--- average.py
+++ average.py
@@ -1,8 +1,5 @@
 # average of five numbers
-a = (4, 5, 1, 2, 3)
-sum = (4+5+1+2+3)
-print(sum)
-
+numbers = [4, 5, 1, 2, 3, 20]
+average = sum(numbers)/len(numbers)
+print(round(average, 5))
 # average will be sum divided by total list of a
-average = sum/5
-print(average)
